b0158da1c8b106bcf1da533cfa3aa35888c36d59
monster199exe <129100396+monster199exe@users.noreply.github.com>
2023-03-30 21:16:55 +0300

    1урок C#

---
+++ Destroy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Команда для удаления
+       Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
